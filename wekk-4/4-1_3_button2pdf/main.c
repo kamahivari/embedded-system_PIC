fbdc0caa92fe2f5d03175fa360612417f4e1de13
Onur Doğan <onur_nagod@outlook.com>
2024-04-10 20:59:03 +0300

    w-4

---
+++ wekk-4/4-1_3_button2pdf/main.c
@@ -0,0 +1,42 @@
+#include <main.h>
+#fuses XT,NOWDT,NOPROTECT,NOBROWNOUT,NOLVP,NOPUT,NOWRT,NODEBUG
+
+#use delay(clock=4M)
+
+void main()
+{
+
+   set_tris_b(0x00);
+   set_tris_d(0x03); //2 buton var artik o yüzden 0x03 verildi
+   int x=1;
+   output_b(0); //baslangicta tum LED ler sonuk olsun
+   
+   
+   while(TRUE)
+   {
+      //TODO: User Code
+   
+      if(input(pin_d0)==1){
+            x=x*2;
+            output_b(x);
+            //200 ms ile butona seri seri basmam 200ms den daha hizli o yuzden
+            //LEDler 2-3 lü atlayarak yaniyor bu gecikmeyi artiralim
+            //delay_ms(200);
+            delay_ms(500);
+            if(x==256){x=128;}
+      }
+      
+      if(input(pin_d1)==1){
+         x=x/2;
+         output_b(x);
+         //delay_ms(200);
+         delay_ms(500);
+         if(x<1){x=1;}
+      }
+   }
+}
+   
+
+
+
+
